395a293a60e069eaaff9d260e33016917d04a952
Radoslav Čerhák <r.cerhak@gmail.com>
2021-11-25 18:10:57 +0100

    add firebase.ts

---
+++ src/utils/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyA04mYkFg3-fRXuT6s68BNZg-c4XDV2k-s',
+	authDomain: 'solaris-pv247.firebaseapp.com',
+	projectId: 'solaris-pv247',
+	storageBucket: 'solaris-pv247.appspot.com',
+	messagingSenderId: '928105868091',
+	appId: '1:928105868091:web:58a11205bebd365ee5e51e'
+};
+
+initializeApp(firebaseConfig);
