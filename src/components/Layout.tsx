8c5cc7b3c66a53b474000dcaf07ce4e78fc26926
Radoslav Čerhák <r.cerhak@gmail.com>
2021-11-25 22:10:43 +0100

    show buttons for logged in / logged out users

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,46 +1,55 @@
 import React, { FC } from 'react';
 import { AppBar, Container, Toolbar, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import useLoggedInUser from '../hooks/useLoggedInUser';
 
 import MainMenu from './MainMenu';
 import UserMenu from './UserMenu';
 
-const Layout: FC = ({ children }) => (
-	<>
-		<AppBar position="fixed">
-			<Container maxWidth="sm">
-				<Toolbar>
-					<MainMenu />
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ flexGrow: 1 }}
-						color="primary"
-					>
-						SOLARIS
-					</Typography>
-					{/*TODO show if user is not logged in*/}
-					<Button>Login</Button>
-					{/*TODO show if user is logged in*/}
-					<UserMenu />
-				</Toolbar>
-			</Container>
-		</AppBar>
+const Layout: FC = ({ children }) => {
+	const user = useLoggedInUser();
 
-		<Container
-			maxWidth="sm"
-			component="main"
-			sx={{
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100vh',
-				pt: 8,
-				gap: 2
-			}}
-		>
-			{children}
-		</Container>
-	</>
-);
+	return (
+		<>
+			<AppBar position="fixed">
+				<Container maxWidth="sm">
+					<Toolbar>
+						<MainMenu />
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{ flexGrow: 1 }}
+							color="primary"
+						>
+							SOLARIS
+						</Typography>
+						{!user && (
+							<Button component={Link} to="/login">
+								Login
+							</Button>
+						)}
+						{user && <UserMenu />}
+					</Toolbar>
+				</Container>
+			</AppBar>
+
+			<Container
+				maxWidth="sm"
+				component="main"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+					pt: 8,
+					gap: 2
+				}}
+			>
+				{children}
+			</Container>
+		</>
+	);
+};
 export default Layout;
