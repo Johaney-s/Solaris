f8c24c1589b86e2d41d3fc3719ea9fc4d04dc1d0
Johana Supíková <johana.supikova@seznam.cz>
2021-12-01 21:58:36 +0100

    Initial search functionality

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,41 +1,87 @@
 import { Search } from '@mui/icons-material';
-import { Box, IconButton, InputAdornment, TextField } from '@mui/material';
-import { FormEvent, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import {
+	Box,
+	Button,
+	Grid,
+	IconButton,
+	InputAdornment,
+	TextField,
+	Tooltip,
+	Typography
+} from '@mui/material';
+import { FormEvent, useEffect, useState } from 'react';
+
+import SpaceBody, { Body } from '../components/SpaceBody';
 
 const SearchForm = () => {
-	const [params, setParams] = useSearchParams();
+	const [params, setParams] = useState<string>('');
+	const [query, setQuery] = useState<string>('');
+	const [data, setData] = useState<Body>();
+	const [error, setError] = useState<string>('');
 
-	// State
-	const [query, setQuery] = useState(params.get('q') ?? '');
-
-	// Handlers
 	const onSubmit = (e: FormEvent) => {
 		e.preventDefault();
-		setParams({ q: query });
+		setParams(query ? query : '');
 	};
 
+	const fetchById = async (id: string) => {
+		fetch(`https://api.le-systeme-solaire.net/rest/bodies/${id}`)
+			.then(response => response.json())
+			.then((response: Body) => setData(response))
+			.catch(error => setError(error));
+	};
+
+	useEffect(() => {
+		setData(undefined);
+		setError('');
+		params && fetchById(params);
+	}, [params]);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
 	return (
-		<Box component="form" onSubmit={onSubmit} sx={{ width: '100%' }}>
-			<TextField
-				id="query"
-				label="Search"
-				fullWidth
-				variant="standard"
-				value={query}
-				onChange={e => setQuery(e.target.value)}
-				InputProps={{
-					endAdornment: (
-						<InputAdornment position="end">
-							<IconButton type="submit" size="large">
-								<Search />
-							</IconButton>
-						</InputAdornment>
-					),
-					sx: { typography: 'h3' }
-				}}
-			/>
-		</Box>
+		<>
+			<Box component="form" onSubmit={onSubmit} sx={{ width: '60%' }}>
+				<TextField
+					id="query"
+					label="Search"
+					variant="standard"
+					value={query}
+					onChange={e => setQuery(e.target.value)}
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+								<IconButton type="submit" size="large">
+									<Search />
+								</IconButton>
+							</InputAdornment>
+						),
+						sx: { typography: 'h3' }
+					}}
+				/>
+			</Box>
+			{data ? (
+				<Grid container justifyContent="center" spacing={2}>
+					<Grid item xs={4}>
+						<SpaceBody body={data} />
+						<Tooltip title={data.isPlanet ? 'Cannot adopt planet' : ''}>
+							<span>
+								<Button disabled={data.isPlanet}>
+									ADOPT {data.englishName}
+								</Button>
+							</span>
+						</Tooltip>
+					</Grid>
+				</Grid>
+			) : null}
+			{error ? (
+				<Typography variant="h5" align="center" color="#ff604f">
+					Object with id {params} not found.
+				</Typography>
+			) : null}
+		</>
 	);
 };
 
--- src/pages/Adopt.tsx
+++ src/pages/Adopt.tsx
@@ -1,24 +1,17 @@
 import { Typography } from '@mui/material';
-import { useSearchParams } from 'react-router-dom';
 
 import SearchForm from '../components/SearchForm';
 
-const Adopt = () => {
-	const [params] = useSearchParams();
-	const query = params.get('q');
-	return (
-		<>
-			<Typography variant="h3" align="center">
-				Adopt an asteroid!
-			</Typography>
-			<Typography variant="body1" mt={5} mb={3}>
-				Adopt your asteroid now. Enter the indentifier or let us pick one for
-				you.
-			</Typography>
-			<SearchForm />
-			{/* {query && <SearchResults query={query} />} */}
-		</>
-	);
-};
+const Adopt = () => (
+	<>
+		<Typography variant="h4" align="center">
+			Adopt an asteroid!
+		</Typography>
+		{/* <Typography variant="body1" mt={5} mb={3}>
+			Adopt your asteroid now. Enter the indentifier or let us pick one for you.
+		</Typography> */}
+		<SearchForm />
+	</>
+);
 
 export default Adopt;
